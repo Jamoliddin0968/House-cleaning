7e12bc571745a0014d29ad1b2c39ff608e071dff
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2023-11-08 10:11:30 +0500

    add swagger

--- config/settings.py
+++ config/settings.py
@@ -140,7 +140,7 @@ AUTH_USER_MODEL = 'users.User'
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
+        "LOCATION": "redis://red-cgrf4bvdvk4n7brrqn1g:6379",
     }
 }
 
